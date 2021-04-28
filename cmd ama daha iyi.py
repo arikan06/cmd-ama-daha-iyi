d0d7f069f73d4244525e9f94723bdea82423df09
mertfsmal <ar_data@hotmail.com>
2021-04-28 18:43:07 +0300

    cmdKomut Türkçe karakterleri ingilizceye çevirme

--- cmd ama daha iyi.py
+++ cmd ama daha iyi.py
@@ -5,18 +5,44 @@ while True:
     try:
         print('----------------------------')
         cmdKomut=input()
-        print('----------------------------')
-        if cmdKomut == 'dizin':
+        cmdKomut = cmdKomut.lower()
+        yeniCmdKomut1 = cmdKomut.replace("ı", "i")
+        yeniCmdKomut2 = yeniCmdKomut1.replace("ş", "s")
+        yeniCmdKomut3 = yeniCmdKomut2.replace("ç", "c")
+        yeniCmdKomut4 = yeniCmdKomut3.replace("ğ", "g")
+        yeniCmdKomut5 = yeniCmdKomut4.replace("ö", "o")
+        yeniCmdKomut = yeniCmdKomut5.replace("ü", "u")
+        print(f"yeni string: {yeniCmdKomut}")
+        if yeniCmdKomut == 'normal cmd':
+            while True:
+                try:
+                    normalCmd = input()
+                    if normalCmd == 'cikis':
+                        break
+                    if normalCmd != 'cikis':
+                        subprocess.call(normalCmd, shell=True)
+                except Exception as e:
+                    print(e)
+        if yeniCmdKomut == 'yardim':
+            print('Buraya baya şey yazcam şimdilik boş')
+        if yeniCmdKomut == 'baslat':
+            subprocess.call('start', shell=True)
+        if yeniCmdKomut == 'gecmis':
+            subprocess.call('doskey /history', shell=True)
+        if yeniCmdKomut == 'dizin':
             subprocess.call('dir', shell=True)
-        if cmdKomut == 'agac':
+        if yeniCmdKomut == 'baslik':
+            sa = input()
+            subprocess.call(f'title {sa}', shell=True)
+        if yeniCmdKomut == 'agac':
             subprocess.call('tree', shell=True)
-        if cmdKomut == 'temizle':
+        if yeniCmdKomut == 'temizle':
             subprocess.call('cls', shell=True)
-        if cmdKomut == 'ip bilgileri':
+        if yeniCmdKomut == 'ip bilgileri':
             subprocess.call('ipconfig', shell=True)
-        if cmdKomut == 'wifi bilgileri':
+        if yeniCmdKomut == 'wifi bilgileri':
             subprocess.call('netsh wlan show profiles', shell=True)
-        if cmdKomut == 'wifi sifreleri':
+        if yeniCmdKomut == 'wifi sifreleri':
             data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8', errors="backslashreplace").split('\n')
             profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
             for i in profiles:
@@ -30,6 +56,6 @@ while True:
                 except subprocess.CalledProcessError:
                     print ("{:<30}|  {:<}".format(i, "ENCODING ERROR"))
     except Exception as e:
-        print('Bu kod programa kayıtlı değildir. Lütfen tekrar deneyin.')
+        print('Bu kod programa kayıtlı değildir. Lütfen tekrar deneyin.', e)
         pass
-        #İngilizce kodları da açmayı ve
+        #İngilizce kodları da açmayı ve string split
