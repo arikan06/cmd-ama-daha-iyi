c4e2b77060737352dda83f642089de6c659e063b
mertfsmal <ar_data@hotmail.com>
2021-04-30 02:11:00 +0300

    pip + ucuncuKomut + sil

--- cmd ama daha iyi.py
+++ cmd ama daha iyi.py
@@ -1,4 +1,5 @@
 #komut tanımlama #İngilizce kodları da açmayı ve string split programı yeniden başlatma matrix hesap makinesi açma
+import pip
 import subprocess
 print('cmd ama daha iyi mertfsmal')
 while True:
@@ -13,22 +14,39 @@ while True:
         yeniCmdKomut5 = yeniCmdKomut4.replace("ö", "o")
         yeniCmdKomut = yeniCmdKomut5.replace("ü", "u")
 
-        if len(yeniCmdKomut.split()) >= 2:
+        if len(yeniCmdKomut.split()) >= 3:
+            ilkKomut, ikinciKomut, ucuncuKomut = yeniCmdKomut.split(' ', 2)
+        elif len(yeniCmdKomut.split()) == 2:
             ilkKomut, ikinciKomut = yeniCmdKomut.split(' ', 1)
         else:
             ilkKomut = yeniCmdKomut
 
+        if ilkKomut == "sil":
+            try:
+                subprocess.call(f"del {ikinciKomut}", shell= True)
+            except NameError:
+                print('Yanlış kullanım. Doğru kullanım: "sil {silmek istediğiniz dosya}"')
+        if ilkKomut == 'pip' and ikinciKomut == 'install':
+            try:
+                pip.main(['install', ucuncuKomut])
+            except Exception as e:
+                print(e)
+                input()
+        if ilkKomut == 'ac':
+            try:
+                subprocess.call(f'cd {ikinciKomut}', shell = True)
+            except Exception as e:
+                print('Yanlış kullanım. Doğru kullanım: "ac {açmak istediğiniz dizin}"')
+                pass
         if ilkKomut == 'baslik':
             try:
                 subprocess.call(f'title {ikinciKomut}', shell = True)
-                print('Başarılı')
             except Exception as e:
                 print('Yanlış kullanım. Doğru kullanım: "Başlık {başlığınız}"')
                 pass
         if ilkKomut == 'renk':
             try:
                 subprocess.call(f'color {ikinciKomut}', shell = True)
-                print('Başarılı')
             except Exception as e:
                 print('Yanlış kullanım. Doğru kullanım: "renk {dilediğiniz renk}"')
                 pass
@@ -49,7 +67,11 @@ while True:
         if ilkKomut == 'gecmis':
             subprocess.call('doskey /history', shell=True)
         if ilkKomut == 'dizin':
-            subprocess.call('dir', shell=True)
+            try:
+                if ikinciKomut == 'olustur':
+                    subprocess.call(f'mkdir {ucuncuKomut}', shell = True)
+            except NameError:
+                subprocess.call('dir', shell=True)
         if ilkKomut == 'agac':
             subprocess.call('tree', shell=True)
         if ilkKomut == 'temizle':
@@ -57,20 +79,21 @@ while True:
         if ilkKomut == 'ip bilgileri':
             subprocess.call('ipconfig', shell=True)
         if ilkKomut == 'wifi':
-            if ikinciKomut == 'sifreler':
-                data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8', errors="backslashreplace").split('\n')
-                profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
-                for i in profiles:
-                    try:
-                        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8', errors="backslashreplace").split('\n')
-                        results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+            try:
+                if ikinciKomut == 'sifreler':
+                    data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8', errors="backslashreplace").split('\n')
+                    profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+                    for i in profiles:
                         try:
-                            print ("{:<30}|  {:<}".format(i, results[0]))
-                        except IndexError:
-                            print ("{:<30}|  {:<}".format(i, ""))
-                    except subprocess.CalledProcessError:
-                        print ("{:<30}|  {:<}".format(i, "ENCODING ERROR"))
-            if ikinciKomut == 'bilgiler':
+                            results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8', errors="backslashreplace").split('\n')
+                            results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+                            try:
+                                print ("{:<30}|  {:<}".format(i, results[0]))
+                            except IndexError:
+                                print ("{:<30}|  {:<}".format(i, ""))
+                        except subprocess.CalledProcessError:
+                            print ("{:<30}|  {:<}".format(i, "ENCODING ERROR"))
+            except Exception as e:
                 subprocess.call('netsh wlan show profiles', shell=True)
     except Exception as e:
         print('Bu kod programa kayıtlı değildir. Lütfen tekrar deneyin.', e)
