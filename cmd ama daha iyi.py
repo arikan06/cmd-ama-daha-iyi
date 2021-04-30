593b252e5ac20fb7f4a37ecaaee91c805c44a3e8
mertfsmal <ar_data@hotmail.com>
2021-04-30 22:16:09 +0300

    yeni motor

--- cmd ama daha iyi.py
+++ cmd ama daha iyi.py
@@ -1,66 +1,65 @@
 #komut tanımlama #İngilizce kodları da açmayı ve string split programı yeniden başlatma matrix hesap makinesi açma windows çalıştır + windows çalıştır recent komudu, return ne ona bakcaz
-import pip
-import subprocess
-print('cmd ama daha iyi mertfsmal')
-while True:
-    try:
+try:
+    import pip
+    import subprocess
+    import time
+except Exception as e:
+    print('Pythonun içinde kurulu olan modüller bulunamadı. Pythonu silip tekrar yüklemeyi deneyin.')
+    input()
+
+print("""                      _    __                     _
+                     | |  / _|                   | |
+  _ __ ___   ___ _ __| |_| |_ ___ _ __ ___   __ _| |
+ | '_ ` _ \ / _ \ '__| __|  _/ __| '_ ` _ \ / _` | |
+ | | | | | |  __/ |  | |_| | \__ \ | | | | | (_| | |
+ |_| |_| |_|\___|_|   \__|_| |___/_| |_| |_|\__,_|_|
+
+                                                    """)
+time.sleep(0.2)
+print('Versiyon: 2.0.0')
+time.sleep(0.2)
+print('Github: github.com/mertfsmal')
+time.sleep(0.2)
+print('Bütün komutları görmek için "yardım" yazın.')
+print()
+time.sleep(0.2)
+
+try:
+    while True:
         print('----------------------------')
-        cmdKomut=input()
-        cmdKomut = cmdKomut.lower()
-        yeniCmdKomut1 = cmdKomut.replace("ı", "i")
-        yeniCmdKomut2 = yeniCmdKomut1.replace("ş", "s")
-        yeniCmdKomut3 = yeniCmdKomut2.replace("ç", "c")
-        yeniCmdKomut4 = yeniCmdKomut3.replace("ğ", "g")
-        yeniCmdKomut5 = yeniCmdKomut4.replace("ö", "o")
-        yeniCmdKomut = yeniCmdKomut5.replace("ü", "u")
+        cmdKomutDuzenlenmemis=input('-> ')
 
-        if len(yeniCmdKomut.split()) >= 3:
-            birinciKomut, ikinciKomut, ucuncuKomut = yeniCmdKomut.split(' ', 2)
-        elif len(yeniCmdKomut.split()) == 2:
-            birinciKomut, ikinciKomut = yeniCmdKomut.split(' ', 1)
-        else:
-            birinciKomut = yeniCmdKomut
+        def stringDuzenle():
+            print('string düzenle girildi')
+            global cmdKomutDuzenlenmemis
+            cmdKomutDuzenlenmemis = cmdKomutDuzenlenmemis.lower()
+            yeniCmdKomut1 = cmdKomutDuzenlenmemis.replace("ı", "i")
+            yeniCmdKomut2 = yeniCmdKomut1.replace("ş", "s")
+            yeniCmdKomut3 = yeniCmdKomut2.replace("ç", "c")
+            yeniCmdKomut4 = yeniCmdKomut3.replace("ğ", "g")
+            yeniCmdKomut5 = yeniCmdKomut4.replace("ö", "o")
+            cmdKomutDuzenlenmis = yeniCmdKomut5.replace("ü", "u")
+            print('string düzenlendi')
 
-        if birinciKomut == "sil":
-            try:
-                subprocess.call(f"del {ikinciKomut}", shell= True)
-            except NameError:
-                print('Yanlış kullanım. Doğru kullanım: "sil {silmek istediğiniz dosya}"')
-        if birinciKomut == 'pip':
-            try:
-                if ikinciKomut == 'indir':
-                    pip.main(['install', ucuncuKomut])
-            except Exception as e:
-                print('Yanlış kullanım. Doğru kullanım: "pip indir {indirmek istediğiniz dosya}"')
-                pass
-        if birinciKomut == 'ac':
-            try:
-                subprocess.call(f'cd {ikinciKomut}', shell = True)
-            except Exception as e:
-                print('Yanlış kullanım. Doğru kullanım: "ac {açmak istediğiniz dizin}"')
-                pass
-        if birinciKomut == 'baslik':
-            try:
-                subprocess.call(f'title {ikinciKomut}', shell = True)
-            except Exception as e:
-                print('Yanlış kullanım. Doğru kullanım: "Başlık {başlığınız}"')
-                pass
-        if birinciKomut == 'renk':
-            try:
-                subprocess.call(f'color {ikinciKomut}', shell = True)
-            except Exception as e:
-                print('Yanlış kullanım. Doğru kullanım: "renk {dilediğiniz renk}"')
-                pass
-        if birinciKomut == 'standart':
+        print('func çağırdık')
+        stringDuzenle()
+        print('Func bitir')
+
+        if len(cmdKomutDuzenlenmis.split()) == 0:
+            print('Girmek istediğiniz komudu girin.')
             while True:
-                try:
-                    normalCmd = input()
-                    if normalCmd == 'cikis':
-                        break
-                    if normalCmd != 'cikis':
-                        subprocess.call(normalCmd, shell=True)
-                except Exception as e:
-                    print(e)
+                komutBekleniyor = input('-> ')
+                if len(cmdKomutDuzenlenmemis.split()) >= 1:
+                    stringDuzenle()
+                    False
+
+        elif len(cmdKomutDuzenlenmis.split()) >= 3:
+            birinciKomut, ikinciKomut, ucuncuKomut = cmdKomutDuzenlenmis.split(' ', 2)
+        elif len(cmdKomutDuzenlenmis.split()) == 2:
+            birinciKomut, ikinciKomut = cmdKomutDuzenlenmis.split(' ', 1)
+        else:
+            birinciKomut = cmdKomutDuzenlenmis
+
         if birinciKomut == 'yardim':
             print("""# Programda bulunan komutlar (1.0):
 * pip             = Python modüllerini indirmeyi sağlar.
@@ -80,45 +79,6 @@ while True:
 * temizle         = Ekranı temizler.
 * ip              = Ip bilgilerinizi gösterir.
 * yardim          = Üstteki komutları programda görmenizi sağlar""")
-        if birinciKomut == 'baslat':
-            subprocess.call('start', shell=True)
-        if birinciKomut == 'gecmis':
-            subprocess.call('doskey /history', shell=True)
-        if birinciKomut == 'dizin':
-            try:
-                if ikinciKomut == 'olustur':
-                    subprocess.call(f'mkdir {ucuncuKomut}', shell = True)
-            except NameError:
-                subprocess.call('dir', shell=True)
-        if birinciKomut == 'agac':
-            subprocess.call('tree', shell=True)
-        if birinciKomut == 'temizle':
-            subprocess.call('cls', shell=True)
-        if birinciKomut == 'ip':
-            subprocess.call('ipconfig', shell=True)
-        if birinciKomut == 'wifi':
-            try:
-                if ikinciKomut == 'detayli':
-                    try:
-                        subprocess.call(f"netsh wlan show profiles {ucuncuKomut} key=clear")
-                    except NameError:
-                        print('Yanlış kullanım. Doğru kullanım: "wifi detayli {wifi adı}"')
-                if ikinciKomut == 'sifreler':
-                    data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8', errors="backslashreplace").split('\n')
-                    profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
-                    for i in profiles:
-                        try:
-                            results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8', errors="backslashreplace").split('\n')
-                            results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
-                            try:
-                                print ("{:<30}|  {:<}".format(i, results[0]))
-                            except IndexError:
-                                print ("{:<30}|  {:<}".format(i, ""))
-                        except subprocess.CalledProcessError:
-                            print ("{:<30}|  {:<}".format(i, "ENCODING ERROR"))
-            except NameError as e:
-                subprocess.call(f'netsh wlan show profiles', shell=True)
-    except Exception as e:
-        print('Bu kod programa kayıtlı değildir. Lütfen tekrar deneyin.', e)
-        pass
-        #İngilizce kodları da açmayı ve string split
+except Exception as e:
+    print(e)
+    input()
