4cfbf4474f50d83d53b201cc6461eea71821573d
mertfsmal <ar_data@hotmail.com>
2021-05-06 16:34:28 +0300

    motor bitti json komutları ve wifi şifreleri falan ekleyip bitiriyorum

--- cmd ama daha iyi.py
+++ cmd ama daha iyi.py
@@ -1,4 +1,6 @@
 #komut tanımlama #İngilizce kodları da açmayı ve string split programı yeniden başlatma matrix hesap makinesi açma windows çalıştır + windows çalıştır recent komudu, return ne ona bakcaz
+# eğer title "title"
+#reboot komudu
 try:
     import pip
     import subprocess
@@ -7,8 +9,6 @@ try:
 except Exception as e:
     print('Pythonun içinde kurulu olan modüller bulunamadı. Pythonu silip tekrar yüklemeyi deneyin.')
     input()
-
-
 
 try:
     with open("komutlar.json", encoding='utf-8', errors='ignore') as f:
@@ -27,7 +27,8 @@ except FileNotFoundError as e:
         print('----------------------------------------------')
         pass
 except Exception as e:
-    print(f"hata: {e}")
+    print(e)
+    input()
 time.sleep(0.06)
 print("""                       _    __                     _""")
 time.sleep(0.06)
@@ -43,7 +44,7 @@ print("""  |_| |_| |_|\___|_|   \__|_| |___/_| |_| |_|\__,_|_|""")
 time.sleep(0.06)
 print()
 time.sleep(0.3)
-print('Versiyon: 2.0.3')
+print('Versiyon: 3')
 time.sleep(0.3)
 print('Github: github.com/mertfsmal')
 time.sleep(0.3)
@@ -55,51 +56,89 @@ baslikYardim = 'Yanlış kullanım. Doğru kullanım "başlık {başlığınız}
 yardimYardim = 'asdasd'
 try:
     def stringDuzenle(duzenlenecekString):
-        duzenlenecekString = duzenlenecekString.lower()
-        yeniCmdKomut1 = duzenlenecekString.replace("ı", "i")
-        yeniCmdKomut2 = yeniCmdKomut1.replace("ş", "s")
-        yeniCmdKomut3 = yeniCmdKomut2.replace("ç", "c")
-        yeniCmdKomut4 = yeniCmdKomut3.replace("ğ", "g")
-        yeniCmdKomut5 = yeniCmdKomut4.replace("ö", "o")
-        duzenlenecekString = yeniCmdKomut5.replace("ü", "u")
-        return duzenlenecekString
+        if duzenlenecekString.startswith('"') and duzenlenecekString.endswith('"'):
+            if len(duzenlenecekString) == 2:
+                print('çift tırnağın içini doldurmalısınız.')
+                duzenlenecekString = '[boş]'
+                return duzenlenecekString
+            if len(duzenlenecekString) >= 3:
+                duzenlenecekString = duzenlenecekString.replace('"', '')
+                return duzenlenecekString
+        else:
+            duzenlenecekString = duzenlenecekString.replace("İ", "i")
+            duzenlenecekString = duzenlenecekString.lower()
+            duzenlenecekString = duzenlenecekString.replace("ı", "i")
+            duzenlenecekString = duzenlenecekString.replace("ş", "s")
+            duzenlenecekString = duzenlenecekString.replace("ç", "c")
+            duzenlenecekString = duzenlenecekString.replace("ğ", "g")
+            duzenlenecekString = duzenlenecekString.replace("ö", "o")
+            duzenlenecekString = duzenlenecekString.replace("ü", "u")
+            duzenlenecekString = duzenlenecekString.replace("baslik", "title")
+            duzenlenecekString = duzenlenecekString.replace("ip", "ipconfig")
+            duzenlenecekString = duzenlenecekString.replace("zaman", "date")
+            duzenlenecekString = duzenlenecekString.replace("tarih", "date")
+            duzenlenecekString = duzenlenecekString.replace("saat", "date")
+            duzenlenecekString = duzenlenecekString.replace("bilgi", "cmd")
+            duzenlenecekString = duzenlenecekString.replace("temizle", "cls")
+            duzenlenecekString = duzenlenecekString.replace("agac", "tree")
+            duzenlenecekString = duzenlenecekString.replace("yeni", "start")
+            return duzenlenecekString
 except Exception as e:
     print('stringDuzenle hata', e)
     input()
 
 try:
+    def ozellik():
+        print('Buraya ekstra kodları eklicem')
+except Exception as e:
+    print(e)
+    input()
+
+try:
     while True:
-        print('----------------------------')
-        cmdKomutDuzenlenmemis=input('->  ')
-        if len(cmdKomutDuzenlenmemis.split()) == 0:
+        cmdKomut=input('->  ')
+        if len(cmdKomut.split()) == 0:
             print('Çalıştırmak istediğiniz komudu girin.')
             while True:
-                cmdKomutDuzenlenmemis=input('->  ')
-                if len(cmdKomutDuzenlenmemis.split()) != 0:
+                cmdKomut=input('->  ')
+                if len(cmdKomut.split()) != 0:
                     break
-        if len(cmdKomutDuzenlenmemis.split()) >= 3:
-            #ikinciUzunKomut = None
-            birinciKomut, ikinciKomut, ucuncuKomut = cmdKomutDuzenlenmemis.split(' ', 2)
-            birinciKomut = stringDuzenle(birinciKomut)
-        if len(cmdKomutDuzenlenmemis.split()) >= 2:
-            #ikinciKomut, ucuncuKomut, = None
-            birinciKomut, ikinciUzunKomut = cmdKomutDuzenlenmemis.split(' ', 1)
-            birinciKomut = stringDuzenle(birinciKomut)
-        if len(cmdKomutDuzenlenmemis.split()) == 1:
-            #ikinciUzunKomut, ikinciKomut, ucuncuKomut, = None
-            birinciKomut = stringDuzenle(cmdKomutDuzenlenmemis)
-
-
-        if birinciKomut == 'baslik':
-            try:
-                subprocess.call(f'title {ikinciUzunKomut}', shell = True)
-            except NameError:
-                print(baslikYardim)
-            except Exception as e:
-                print(e)
-                input()
-        if birinciKomut == 'yardim':
-            print(komutlarJsonPrint)
+        if len(cmdKomut.split()) == 1:
+            birinciKomut=cmdKomut
+            birinciKomut=stringDuzenle(birinciKomut)
+            if birinciKomut.startswith('_'):
+                ozellik()
+            else:
+                subprocess.call(birinciKomut, shell=True)
+        if len(cmdKomut.split()) == 2:
+            birinciKomut, ikinciKomut=cmdKomut.split()
+            birinciKomut=stringDuzenle(birinciKomut)
+            ikinciKomut=stringDuzenle(ikinciKomut)
+            if birinciKomut.startswith('_'):
+                ozellik()
+            else:
+                subprocess.call(f"{birinciKomut} {ikinciKomut}", shell=True)
+        if len(cmdKomut.split()) == 3:
+            birinciKomut, ikinciKomut, ucuncuKomut=cmdKomut.split()
+            birinciKomut=stringDuzenle(birinciKomut)
+            ikinciKomut=stringDuzenle(ikinciKomut)
+            ucuncuKomut=stringDuzenle(ucuncuKomut)
+            if birinciKomut.startswith('_'):
+                ozellik()
+            else:
+                subprocess.call(f"{birinciKomut} {ikinciKomut} {ucuncuKomut}", shell=True)
+        if len(cmdKomut.split()) == 4:
+            birinciKomut, ikinciKomut, ucuncuKomut, dorduncuKomut=cmdKomut.split()
+            birinciKomut=stringDuzenle(birinciKomut)
+            ikinciKomut=stringDuzenle(ikinciKomut)
+            ucuncuKomut=stringDuzenle(ucuncuKomut)
+            dorduncuKomut=stringDuzenle(dorduncuKomut)
+            if birinciKomut.startswith('_'):
+                ozellik()
+            else:
+                subprocess.call(f"{birinciKomut} {ikinciKomut} {ucuncuKomut} {dorduncuKomut}", shell=True)
+        if len(cmdKomut.split()) >= 5:
+            print('En fazla 4 kelime yazabilirsiniz.')
 except Exception as e:
     print(e)
     input()
