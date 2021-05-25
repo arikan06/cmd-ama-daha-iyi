28c552970319c8dceb4db29262134bfdafe4261d
mertfsmal <mertfsmal@gmail.com>
2021-05-25 16:17:31 +0300

    boş exceptionları çıkardım, motor son

--- cmd ama daha iyi.py
+++ cmd ama daha iyi.py
@@ -44,10 +44,16 @@ print('Bütün komutları görmek için "_komutlar" yazın.')
 print()
 time.sleep(0.3)
 
-baslikYardim = 'Yanlış kullanım. Doğru kullanım "başlık {başlığınız}"'
-yardimYardim = 'asdasd'
-try:
-    def stringDuzenle(duzenlenecekString):
+def stringDuzenle(duzenlenecekString):
+    if duzenlenecekString.startswith('"') and duzenlenecekString.endswith('"'):
+        if len(duzenlenecekString) == 2:
+            print('çift tırnağın içini doldurmalısınız.')
+            duzenlenecekString = '[boş]'
+            return duzenlenecekString
+        if len(duzenlenecekString) >= 3:
+            duzenlenecekString = duzenlenecekString.replace('"', '')
+            return duzenlenecekString
+    else:
         duzenlenecekStringTurkce = duzenlenecekString
         duzenlenecekString = duzenlenecekString.replace("İ", "i")
         duzenlenecekString = duzenlenecekString.lower()
@@ -68,44 +74,22 @@ try:
         duzenlenecekString = duzenlenecekString.replace("temizle", "cls")
         duzenlenecekString = duzenlenecekString.replace("agac", "tree")
         duzenlenecekString = duzenlenecekString.replace("yeni", "start")
-        duzenlenecekString = duzenlenecekString.replace("ac", "cd")
-        duzenlenecekString = duzenlenecekString.replace("dizin olustur", "mkdir")
         duzenlenecekString = duzenlenecekString.replace("dizin", "dir")
+        duzenlenecekString = duzenlenecekString.replace("ac", "cd")
         if duzenlenecekStringDegistiMi == duzenlenecekString:
             duzenlenecekString = duzenlenecekStringTurkce
             return duzenlenecekString
         else:
             return duzenlenecekString
-except Exception as e:
-    print('stringDuzenle hata', e)
-    input()
 
-try:
-    def kontrol(kontrolEdilecek):
-        if len(kontrolEdilecek.split()) >= komutlarJson['komutlar'][0][kontrolEdilecek][0]['gereksinim']:
-            kontrolDegisken = True
-            return kontrolDegisken
-        else:
-            kontrolDegisken = False
-            return kontrolDegisken
-except Exception as e:
-    print('kontrol def hata', e)
-    input()
-
-try:
-    def komut(komutDegisken):
-        komutDegisken = str(komutDegisken)
-        komutDegisken = komutDegisken.replace('_', '', 1)
-        #kontrol(komutDegisken)
-        if komutDegisken == 'komutlar':
-            print(komutlarJsonPrint)
-        if komutDegisken == 'wifi':
-            print(komutlarJsonPrint)
-except Exception as e:
-    print('komut bulunamadı bütün komutları görmek için _komutlar yazın.')
-    input()
-    pass
-
+def komut(komutDegisken):
+    komutDegisken = str(komutDegisken)
+    komutDegisken = komutDegisken.replace('_', '', 1)
+    kontrol(komutDegisken)
+    if komutDegisken == 'komutlar':
+        print(komutlarJsonPrint)
+    if komutDegisken == 'wifi':
+        print(komutlarJsonPrint)
 try:
     while True:
         cmdKomut=input('->  ')
@@ -115,33 +99,15 @@ try:
                 cmdKomut=input('->  ')
                 if len(cmdKomut.split()) != 0:
                     break
-        if len(cmdKomut.split()) == 1:
-            calistirilacakKomut = stringDuzenle(cmdKomut)
+        if len(cmdKomut.split()) >= 1:
+            calistirilacakKomut = ''
+            for kelime in cmdKomut.split():
+                kelime = stringDuzenle(kelime)
+                calistirilacakKomut += f' {kelime}'
             if calistirilacakKomut.startswith('_'):
                 komut(calistirilacakKomut)
             else:
                 subprocess.call(calistirilacakKomut, shell=True)
-        if len(cmdKomut.split()) == 2:
-            calistirilacakKomut = stringDuzenle(cmdKomut)
-            if calistirilacakKomut.startswith('_'):
-                komut(calistirilacakKomut)
-            else:
-                subprocess.call(calistirilacakKomut, shell=True)
-        if len(cmdKomut.split()) == 3:
-            calistirilacakKomut = stringDuzenle(cmdKomut)
-            if calistirilacakKomut.startswith('_'):
-                komut(calistirilacakKomut)
-            else:
-                subprocess.call(calistirilacakKomut, shell=True)
-        if len(cmdKomut.split()) == 4:
-            calistirilacakKomut = stringDuzenle(cmdKomut)
-            if calistirilacakKomut.startswith('_'):
-                komut(calistirilacakKomut)
-            else:
-                subprocess.call(calistirilacakKomut, shell=True)
-        if len(cmdKomut.split()) >= 5:
-            calistirilacakKomut = stringDuzenle(cmdKomut)
-            subprocess.call(calistirilacakKomut, shell=True)
 except Exception as e:
-    print('main hata', e)
+    print('main hata,', e)
     input()
