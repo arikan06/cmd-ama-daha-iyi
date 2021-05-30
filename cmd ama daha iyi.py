b1d4e4863d3b2acfc49a6eed6fe557bcb6aed9de
mertfsmal <mertfsmal@gmail.com>
2021-05-30 18:25:10 +0300

    .komutlar ingilizce()

--- cmd ama daha iyi.py
+++ cmd ama daha iyi.py
@@ -10,7 +10,7 @@ try:
 except Exception as e:
     print('Pythonun içinde kurulu olan modüller bulunamadı. Pythonu silip tekrar yüklemeyi deneyin.')
     input()
-
+subprocess.call('title cmd ama daha iyi', shell=True)
 try:
     komutlarJson = json.load(open('komutlar.json'))
     komutlarJsonPrint = json.dumps(komutlarJson, indent=1)
@@ -21,20 +21,22 @@ except FileNotFoundError as e:
 except Exception as e:
     print('json hatası', e)
     input()
-time.sleep(0.06)
-print("""                       _    __                     _""")
-time.sleep(0.06)
-print("""                      | |  / _|                   | |""")
-time.sleep(0.06)
-print("""   _ __ ___   ___ _ __| |_| |_ ___ _ __ ___   __ _| |""")
-time.sleep(0.06)
-print("""  | '_ ` _ \ / _ \ '__| __|  _/ __| '_ ` _ \ / _` | |""")
-time.sleep(0.06)
-print("""  | | | | | |  __/ |  | |_| | \__ \ | | | | | (_| | |""")
-time.sleep(0.06)
-print("""  |_| |_| |_|\___|_|   \__|_| |___/_| |_| |_|\__,_|_|""")
-time.sleep(0.06)
-print()
+def mertfsmal():
+    time.sleep(0.06)
+    print("""                       _    __                     _""")
+    time.sleep(0.06)
+    print("""                      | |  / _|                   | |""")
+    time.sleep(0.06)
+    print("""   _ __ ___   ___ _ __| |_| |_ ___ _ __ ___   __ _| |""")
+    time.sleep(0.06)
+    print("""  | '_ ` _ \ / _ \ '__| __|  _/ __| '_ ` _ \ / _` | |""")
+    time.sleep(0.06)
+    print("""  | | | | | |  __/ |  | |_| | \__ \ | | | | | (_| | |""")
+    time.sleep(0.06)
+    print("""  |_| |_| |_|\___|_|   \__|_| |___/_| |_| |_|\__,_|_|""")
+    time.sleep(0.06)
+    print()
+mertfsmal()
 time.sleep(0.3)
 print('Versiyon: 3')
 time.sleep(0.3)
@@ -43,6 +45,17 @@ time.sleep(0.3)
 print('Bütün komutları görmek için ".komutlar" yazın.')
 print()
 time.sleep(0.3)
+
+def ingilizce(ingilizcelestir):
+    ingilizcelestir = ingilizcelestir.replace("İ", "i")
+    ingilizcelestir = ingilizcelestir.lower()
+    ingilizcelestir = ingilizcelestir.replace("ı", "i")
+    ingilizcelestir = ingilizcelestir.replace("ş", "s")
+    ingilizcelestir = ingilizcelestir.replace("ç", "c")
+    ingilizcelestir = ingilizcelestir.replace("ğ", "g")
+    ingilizcelestir = ingilizcelestir.replace("ö", "o")
+    ingilizcelestir = ingilizcelestir.replace("ü", "u")
+    return ingilizcelestir
 
 def stringDuzenle(duzenlenecekString):
     if duzenlenecekString.startswith('"') and duzenlenecekString.endswith('"'):
@@ -55,14 +68,7 @@ def stringDuzenle(duzenlenecekString):
             return duzenlenecekString
     else:
         duzenlenecekStringTurkce = duzenlenecekString
-        duzenlenecekString = duzenlenecekString.replace("İ", "i")
-        duzenlenecekString = duzenlenecekString.lower()
-        duzenlenecekString = duzenlenecekString.replace("ı", "i")
-        duzenlenecekString = duzenlenecekString.replace("ş", "s")
-        duzenlenecekString = duzenlenecekString.replace("ç", "c")
-        duzenlenecekString = duzenlenecekString.replace("ğ", "g")
-        duzenlenecekString = duzenlenecekString.replace("ö", "o")
-        duzenlenecekString = duzenlenecekString.replace("ü", "u")
+        duzenlenecekString = ingilizce(duzenlenecekString)
         duzenlenecekStringDegistiMi = duzenlenecekString
         duzenlenecekString = duzenlenecekString.replace("baslik", "title")
         duzenlenecekString = duzenlenecekString.replace("ip", "ipconfig")
@@ -94,31 +100,35 @@ def stringDuzenle(duzenlenecekString):
             return duzenlenecekString
 
 def komut(komutDegisken):
-    print('NOT: nedense duzenlenecekString func çalışmıyor o yüzden siz küçük karakterlerle ve ingilizce harflerle yazın yoksa program algılamaz.')
-    print()
     komutDegisken = komutDegisken.replace(' .', '', 1)
-    if komutDegisken == 'komutlar':
-        print(komutlarJsonPrint)
-    if komutDegisken == 'wifi':
-        print('wifi şifreler, wifi detayli')
-    if komutDegisken == 'wifi detayli':
-        subprocess.call('netsh wlan show profiles', shell=True)
-        k=input('Bilgisayarda kayıtlı wifi adresleri yukarıdadır. Detaylı bilgi öğrenmek istediklerinizin adresini girin. ')
-        subprocess.call(f'netsh wlan show profiles {k} key=clear', shell=True)
-    if komutDegisken == 'wifi sifreler':
-        data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8', errors="backslashreplace").split('\n')
-        profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
-        for i in profiles:
-            try:
-                results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8', errors="backslashreplace").split('\n')
-                results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
-                try:
+    komutDegisken=ingilizce(komutDegisken)
+    if len(komutDegisken.split()) == 1:
+        komut1 = komutDegisken.split()
+        if komutDegisken == 'mertfsmal':
+            mertfsmal()
+        if komutDegisken == 'komutlar':
+            print(komutlarJsonPrint)
+    if len(komutDegisken.split()) == 2:
+        komut1, komut2 = komutDegisken.split(' ', 1)
+    
+        if komutDegisken == 'wifi detayli':
+            subprocess.call('netsh wlan show profiles', shell=True)
+            k=input('Bilgisayarda kayıtlı wifi adresleri yukarıdadır. Detaylı bilgi öğrenmek istediklerinizin adresini girin. ')
+            subprocess.call(f'netsh wlan show profiles {k} key=clear', shell=True)
+
+        if komutDegisken == 'wifi sifreler':
+            data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8', errors="backslashreplace").split('\n')
+            profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+            for i in profiles:
+                    results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8', errors="backslashreplace").split('\n')
+                    results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
                     print ("{:<30}|  {:<}".format(i, results[0]))
-                except IndexError:
-                    print ("{:<30}|  {:<}".format(i, ""))
-            except subprocess.CalledProcessError:
-                print ("{:<30}|  {:<}".format(i, "ENCODING ERROR"))
-        print()
+        if komut1 == 'komutlar':
+            print(komutlarJson["komutlar"][0][komut2][0])
+    if len(komutDegisken.split()) == 3:
+        komut1, komut2, komut3 = komutDegisken.split(' ', 1)
+    if len(komutDegisken.split()) >= 4:
+        print('komut bulunamadı. Bütün komutlar için ".komutlar" yazın')
 try:
     while True:
         cmdKomut=input('->  ')
